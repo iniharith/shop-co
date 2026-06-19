5e1905f03e031741782d050349f17b487dda540f
iniharith <133095026+iniharith@users.noreply.github.com>
2026-06-19 15:13:01 +0800

    Fix signup routing error by setting Axios fallback baseURL

--- frontend/src/utils/axios.ts
+++ frontend/src/utils/axios.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const AxiosInstance = (token: string="") => {
     return axios.create({
-        baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
+        baseURL: process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:8000",
         withCredentials: true,
         headers: {
             Authorization: `Bearer ${token}`,
