95345a820f7785222ce7b40b255c3bd9041a7628
iniharith <133095026+iniharith@users.noreply.github.com>
2026-06-23 19:20:30 +0800

    Add React error boundary inside ProductDetails to catch calculation crashes

---
+++ test_render.js
@@ -0,0 +1,15 @@
+require('ts-node/register');
+const React = require('react');
+const { renderToString } = require('react-dom/server');
+const { ProductDetails } = require('./frontend/src/components/page-sections/shop/product-details.tsx');
+const { dummyProducts } = require('./frontend/src/constants/dummy-products.ts');
+
+const product = dummyProducts.find(p => p._id === 'prod-141');
+
+try {
+  const html = renderToString(React.createElement(ProductDetails, { product }));
+  console.log('RENDER SUCCESS! HTML length:', html.length);
+} catch (e) {
+  console.error('RENDER CRASHED!');
+  console.error(e);
+}
